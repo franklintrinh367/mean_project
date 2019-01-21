d14b45ecdf3a6afda38da122f8d208e960d1074d
Hao Bui <iamhaobui@gmail.com>
2019-01-20 20:45:43 -0500

    refactor

--- src/app/pipe/first-err-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FirstErrPipePipe } from './first-err-pipe.pipe';
-
-describe('FirstErrPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FirstErrPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
