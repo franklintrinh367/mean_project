3855ca2b1fbf428c3f831d762ceb8b3ed67f5e50
habdallah005 <43259755+habdallah005@users.noreply.github.com>
2019-01-23 13:24:00 -0500

    I created admin-homepages component

---
+++ client/src/app/components/admin-homepages/admin-homepages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-homepages',
+  templateUrl: './admin-homepages.component.html',
+  styleUrls: ['./admin-homepages.component.scss']
+})
+export class AdminHomepagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
