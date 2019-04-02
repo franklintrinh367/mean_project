ad024803f22f349648a64c76774c480cda4e3c6a
franklintrinh367 <franklintrinh367@gmail.com>
2019-04-02 16:23:50 -0400

    Settings - beta : Data from backend for profile-settings

--- src/app/components/main/setting/profile-setting/profile-setting.component.ts
+++ src/app/components/main/setting/profile-setting/profile-setting.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { AuthenticateService } from 'src/app/services/authenticate.service'
 
 @Component({
   selector: 'app-profile-setting',
@@ -6,7 +7,21 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./profile-setting.component.scss'],
 })
 export class ProfileSettingComponent implements OnInit {
-  constructor() {}
+  user: any = {}
+  constructor(private auth: AuthenticateService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    let token = this.returnToken()
+    if (token) this.user = token
+  }
+
+  returnToken() {
+    return this.auth.getTokenDetails('auth-token')
+      ? this.auth.getTokenDetails('auth-token')
+      : null
+  }
+
+  isCandidate() {
+    return this.user.role === 'Candidate'
+  }
 }
