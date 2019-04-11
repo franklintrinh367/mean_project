06e802eca1a66aaef59f03b3216777e0e5f306b8
Tam Dang <danhtam0509@gmail.com>
2019-04-10 22:09:22 -0400

    bugs when deploying

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyApyMZ6t9vFNEPzmIPkqb1AQEcbFo6HVOk',
+    authDomain: 'jc-sonsulting.firebaseapp.com',
+    databaseURL: 'https://jc-sonsulting.firebaseio.com',
+    projectId: 'jc-sonsulting',
+    storageBucket: 'jc-sonsulting.appspot.com',
+    messagingSenderId: '447841530838',
+  },
   url: '',
 }
